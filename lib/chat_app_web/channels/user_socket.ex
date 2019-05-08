90cf21dc6af999fcdc6b00fea3e2e24c3dece439
nogtk <taka.naoga@moneyforward.co.jp>
2019-05-08 12:38:00 +0900

    Create simple chat application

--- lib/chat_app_web/channels/user_socket.ex
+++ lib/chat_app_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChatAppWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChatAppWeb.RoomChannel
+  channel "room:*", ChatAppWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
