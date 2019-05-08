54893c452cb85e9cfed7cd2317ee273d6fac4eca
nogtk <taka.naoga@moneyforward.co.jp>
2019-05-08 14:12:24 +0900

    rename hostname

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :chat_app, ChatAppWeb.Endpoint,
   # url: [host: "example.com", port: 80],
-  url: [scheme: "https", host: "guarded-bayou-61457", port: 443],
+  url: [scheme: "https", host: "kensyu-chatapp", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
