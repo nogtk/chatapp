c0adee6bdbf61b4222fd5426f3e3e9be84267c0d
nogtk <taka.naoga@moneyforward.co.jp>
2019-05-14 13:35:51 +0900

    Added function that user able to create chatroom from web-interface

--- lib/chat_app_web/controllers/room_controller.ex
+++ lib/chat_app_web/controllers/room_controller.ex
@@ -2,13 +2,30 @@ defmodule ChatAppWeb.RoomController do
   use ChatAppWeb, :controller
   alias ChatApp.Room
   alias ChatApp.Repo
-  # def index(conn, %{"username" => username}) do
-  def index(conn, %{"username" => username}) do
+  def enter(conn, %{"username" => username}) do
     rooms = Room |> Repo.all
     render(conn, "index.html", rooms: rooms, username: username)
+  end
+  def index(conn, %{"name" => name}) do
+    rooms = Room |> Repo.all
+    render(conn, "index.html", rooms: rooms, username: name)
   end
   def show(conn, %{"id" => id, "name" => name}) do
     room = Room |> Repo.get(id)
     render(conn, "chatroom.html", room: room, name: name)
+  end
+  def new(conn, %{"name" => name}) do
+    changeset = Room.changeset(%Room{}, %{})
+    render(conn, "new.html", changeset: changeset, name: name)
+  end
+  def create(conn, %{"room" => room_params, "name" => name}) do
+    changeset = Room.changeset(%Room{}, room_params)
+    case Repo.insert(changeset) do
+      {:ok, room} ->
+        conn
+        |> redirect(to: Routes.room_path(conn, :index, name))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset, name: name)
+    end
   end
 end
