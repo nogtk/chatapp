7526ed06f28cecce1e326b0b46a75cd2f8b41f18
nogtk <taka.naoga@moneyforward.co.jp>
2019-05-24 15:48:21 +0900

    Remove the unused code

--- lib/chat_app_web/controllers/page_controller.ex
+++ lib/chat_app_web/controllers/page_controller.ex
@@ -1,9 +1,6 @@
 defmodule ChatAppWeb.PageController do
   use ChatAppWeb, :controller
-  def new(conn, _params) do
-    render(conn, "new.html")
-  end
-  def enter(conn, %{"username" => name}) do
-    render(conn, "index.html", name: name || "名無しさん")
+  def index(conn, _params) do
+    render(conn, "index.html")
   end
 end
